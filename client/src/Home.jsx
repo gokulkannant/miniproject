bdf2317d69ea81e565ef1285d020f09afb049304
gokul <108364852+sv-00@users.noreply.github.com>
2024-05-06 22:16:50 +0530

    css modified

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Home.css';
+import './Home.css'; // Ensure this path correctly points to your CSS file
 
 function Home() {
     return (
@@ -8,24 +8,23 @@ function Home() {
             {/* Navigation bar */}
             <nav className="navbar">
                 <div className="logo">
-                    <img src="/path/to/logo.png" alt="Logo" />
+                    <img src="/logo.svg" alt="Logo" />
                     <span className="logo-text">Campus Navigation</span>
                 </div>
                 <ul className="nav-links">
-                    <li><Link to="/location-finder">Location Finder</Link></li>
-                    <li><Link to="/exam">Exam Hall Finder</Link></li>
                     <li><Link to="/login">Login</Link></li>
                 </ul>
             </nav>
 
             {/* Content */}
             <div className="content">
-                {/* Your content here */}
-                <h2>Welcome to Campus Navigation System</h2><li>
-                    <Link to="/location-finder">Location Finder</Link></li>
-                    <li><Link to="/exam">Exam Hall Finder</Link></li>
-
+                <h2>Welcome to Campus Navigation System</h2>
                 <p>Here you can find various tools to navigate through our campus easily.</p>
+                {/* Additional links for clarity */}
+                <ul className="content-links">
+                    <li><Link to="/location-finder">Location Finder</Link></li>
+                    <li><Link to="/exam">Exam Hall Finder</Link></li>
+                </ul>
             </div>
 
             {/* Contact Us */}
